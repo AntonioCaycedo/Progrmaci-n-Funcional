794cb654e52d41392fae523fb4d2d3432de56d32
AntonioCaycedo <acaycedo@unal.edu.co>
2025-07-16 23:25:33 -0500

    Update and rename Ejemplo Prog. Funcional.phy to Ejemplo Prog. Funcional.py

---
+++ Ejemplo Prog. Funcional.py
@@ -0,0 +1,15 @@
+//Vamos a crear un proyecto de un laboratorio químico, donde documentaremos los productos y proyectos del mismo
+
+def nuevo_proyecto(nombre, area , presupuesto, activo):
+  return{
+    "Tipo":"Proyecto",
+    "Nombre": nombre
+    "Area de estudio": area,
+    "Presupuesto": presupuesto,
+    "Activo": activo
+  }
+  
+def ver_proyecto(nombre):
+  return f"{nombre['nombre']} {nombre[area]}  presupuesto, activo):
+
+def actualizar_proyecto()
